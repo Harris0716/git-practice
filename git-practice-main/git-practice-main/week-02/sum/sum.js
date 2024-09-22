493147d16be3e6bcad25abbe4cedd2ab3ac1516b
Harris0716 <145627737+Harris0716@users.noreply.github.com>
2024-09-23 04:02:45 +0800

    Delete git-practice-main/git-practice-main directory

--- git-practice-main/git-practice-main/week-02/sum/sum.js
+++
@@ -1,9 +0,0 @@
-// ary: number array
-function sum(ary) {
-    return ary.reduce((a, b) => a + b);
-}
-
-console.log(sum([1, 5, 3, 2])); // 11
-
-// reduce是function language的語法，input是一個二元運算(e.g. +,-,*,/)
-// 運算過程可簡化為 reduce([1,5,3,2], + ) -> ((1+5)+3)+2
